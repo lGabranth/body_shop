138bff96fe4a7ff10d5fab253949245698abdef1
kirtsu04 <lucas.ru74@gmail.com>
2021-11-15 15:44:10 +0100

    authenticateFunction

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,8 +4,14 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class UserController extends AbstractController
 {
@@ -19,5 +25,18 @@ class UserController extends AbstractController
       return $this->json([
         'user' => $user->toArray(),
       ]);
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        $password = $request->request->get('password');
+        $username = $request->request->get('username');
+        $csrfToken = $request->request->get('csrf_token');
+
+        return new Passport(
+            new UserBadge($username),
+            new PasswordCredentials($password),
+            [new CsrfTokenBadge('login', $csrfToken)]
+        );
     }
 }
